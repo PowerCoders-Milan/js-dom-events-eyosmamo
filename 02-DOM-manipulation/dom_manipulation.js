66242e628bf193d0a628bf1b1e81c1c06d929cfc
eyosiyas <eyosiyas@AULA1D-DOCENTE.edu-il.it>
2021-06-29 14:29:34 +0200

    dom_manipulation done

--- 02-DOM-manipulation/dom_manipulation.js
+++ 02-DOM-manipulation/dom_manipulation.js
@@ -1,39 +1,45 @@
 // Ex 1. Add the .blue CSS class to the th elements
 
-const thElements = document.querySelectorAll('');
+const thElements = document.querySelectorAll('th');
 
 thElements.forEach((element) => {
-  element.className = "";
+  element.className = "blue";
 });
 
 // Ex 2. Count the number of table body rows there are
 //       Make the function teamCount() return it
 const teamCount = () => {
   // TODO: return the number of teams
-  const trElements = document.querySelectorAll('');
-  return ;
+  const trElements = document.querySelectorAll('tbody tr');
+  return trElements.length;
 };
 
 // Ex 3. Say there is a 15th team added to the table.
 //       Add a row at the bottom, this new team (Spezia) should have 39 points.
 
-const list = document.querySelector('');
-list.insertAdjacentHTML('beforeend', '');
-
+const list = document.querySelector('tbody');
+list.insertAdjacentHTML('beforeend', "<tr><td>15</td><td>Spezia</td><td>39</td></tr>");
+console.log(list);
 // Ex 4. Write some code to sum all points given to all teams
 //       Make the function summarizePoints() return it
 const summarizePoints = () => {
   // TODO: return the sum
-  return ;
-};
+  const list = document.querySelectorAll("tr");
+  let sum = 0;
+  list.forEach((li) => {
+    let td = li.querySelectorAll("tr td");
+    sum += parseInt(td[2]);
+    return sum;
+  })
+}
 
 
 // Ex 5. Change the background color of all `<th>` cells to #DDF4FF
 
-const thEl = document.querySelectorAll('');
+const thEl = document.querySelectorAll('th');
 
 thEl.forEach((element) => {
-  element.style.backgroundColor = "";
+  element.style.backgroundColor = "#DDF4FF";
 });
 
 
